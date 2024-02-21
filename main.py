8c963a615538d2c8224b6218153441980a5d93df
ryanoliveir <ryandehallo229@gmail.com>
2024-02-20 22:52:52 -0300

    add: move demo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WIDTH, HEIGTH = 900, 500
 WHITE = (255,255,255)
 
 FPS = 60
+VELOCITY = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40 
 
 window = pygame.display.set_mode((WIDTH, HEIGTH))
@@ -22,10 +23,10 @@ space_ship_red_image = pygame.image.load(
     os.path.join('assets', 'spaceship_red.png'))
 space_ship_red = pygame.transform.rotate(pygame.transform.scale(space_ship_red_image, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_window():
+def draw_window(player_1, player_2_):
     window.fill(WHITE)
-    window.blit(space_ship_yellow, (300, 100))
-    window.blit(space_ship_red, (700, 100))
+    window.blit(space_ship_yellow, (player_1.x, player_1.y))
+    window.blit(space_ship_red, (player_2_.x, player_2_.y))
     
 
     pygame.display.update()
@@ -34,7 +35,7 @@ def draw_window():
 def main():
     clock = pygame.time.Clock()
 
-    player_red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    player_red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     player_yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
     run = True
@@ -45,7 +46,22 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_a]: #left
+            player_yellow.x -= VELOCITY
+        if keys_pressed[pygame.K_d]: #right
+            player_yellow.x += VELOCITY
+        if keys_pressed[pygame.K_w]: #up
+            player_yellow.y -= VELOCITY
+        if keys_pressed[pygame.K_s]: #down
+            player_yellow.y += VELOCITY
+            
+
+
+        draw_window(player_yellow, player_red)
+
 
     pygame.quit()
 
