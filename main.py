520c304ca2164feb236425ee2e913060477aa101
ryanoliveir <ryandehallo229@gmail.com>
2024-02-20 22:37:52 -0300

    add: spaceship in screen

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
+import os 
 
 WIDTH, HEIGTH = 900, 500
 
 WHITE = (255,255,255)
 
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40 
 
 window = pygame.display.set_mode((WIDTH, HEIGTH))
 
 pygame.display.set_caption("Game")
 
 
+space_ship_yellow_image = pygame.image.load(
+    os.path.join('assets','spaceship_yellow.png'))
+space_ship_yellow = pygame.transform.rotate(pygame.transform.scale(space_ship_yellow_image, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),90)
+
+
+space_ship_red_image = pygame.image.load(
+    os.path.join('assets', 'spaceship_red.png'))
+space_ship_red = pygame.transform.rotate(pygame.transform.scale(space_ship_red_image, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 def draw_window():
     window.fill(WHITE)
+    window.blit(space_ship_yellow, (300, 100))
+    window.blit(space_ship_red, (700, 100))
+    
+
     pygame.display.update()
 
 
